898b9594311e2f09d13f3b818a2b3c8f575738fe
lukabelani <luka.belani@gmail.com>
2021-02-17 23:39:34 +0100

    remove comment and clean up unused code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,26 @@
 import React from "react";
 import { GiAutoRepair } from "react-icons/gi";
 
-const styles = {
+const headerStyles = {
   marginLeft: "20px",
   marginTop: "auto",
   marginBottom: "auto",
   marginRight: "0px",
-  // color: "#0E7172",
   color: "#083d77",
   fontWeight: "900",
 };
 
-export const containerStyles = {
-  display: "block",
-  maxWidth: "100%",
-  textAlign: "center",
-  color: "#083d77",
+const iconStyles = {
+  fontSize: "3.5rem",
+  color: "#da4167",
 };
 
 const Header = () => {
   return (
     <div className="ui left aligned segment">
       <div className="ui header" style={{ display: "flex" }}>
-        <GiAutoRepair style={{ fontSize: "3.5rem", color: "#da4167" }} />
-        <div style={styles}>
+        <GiAutoRepair style={iconStyles} />
+        <div style={headerStyles}>
           <p style={{ marginBottom: "0px" }}>Car Repair</p>
           <p>Service Calculator</p>
         </div>
